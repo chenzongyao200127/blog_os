b0e75931fd913c0df447267793db6b948ddd121a
chenzongyao200127 <2319251792@qq.com>
2022-09-13 20:38:48 +0800

    fix bugs

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ pub extern "C" fn _start() -> ! {
     // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
     // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
     println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
